62556f67b82752a497bd65a7fe839b21a647eba9
Sean Pandaleon <skp226@lehigh.edu>
2025-03-01 17:55:23 -0500

    delete file

--- scripts/prompt.js
+++ scripts/prompt.js
@@ -3,7 +3,6 @@ async function dataToText(data) {
 }
 
 export async function generateContent(prompt) {
-const apiKey = "AIzaSyD0ZDzQgY5olkJc1fOLLC9X3BS6CewBj1E";
 
 return fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
   method: "POST",
