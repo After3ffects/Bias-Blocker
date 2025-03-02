9dfd98949ccaeb7a9bf1636c0d75739a413c366c
After3ffects <121523105+After3ffects@users.noreply.github.com>
2025-03-01 19:53:27 -0500

    minor changes

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,6 @@
 import { generateContent } from "./prompt.js";
 
 
-
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.type === "articleText") {
@@ -11,20 +10,7 @@ chrome.runtime.onMessage.addListener(
       const prompt = `Analyze the following news article for potential bias. Identify specific words and phrases that suggest bias. Return ONLY a javascript object format of these words as keys and then why they suggest bias as values to the keys, enclosed in curly brackets. For example: {"word1" : "why word1 suggests a bias", "phrase2" : "why phrase2 suggests a bias"} Do not include any other text or explanation.:\n\n${articleText}`
       
       generateContent(prompt).then(result => {
-        //console.log("Gemini analysis result:", result);
-        let resultTrimmed = result.substring(result.indexOf('{')-1, result.lastIndexOf('}')+1);
-        console.log(resultTrimmed)
-        let biasedDict = JSON.parse(resultTrimmed);
-        console.log(biasedDict)
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          if (tabs && tabs[0] && tabs[0].id) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-              type: "biasAnalysisResult",
-              data: biasedDict
-            });
-          }
-        });
-
+        console.log("Gemini analysis result:", result);
       })
       .catch(error => {
           console.error("Error analyzing bias with Gemini:", error);
